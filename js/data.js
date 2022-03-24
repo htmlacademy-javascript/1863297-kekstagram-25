b88cd06e664411967f20ed469c399e4a9d950cc4
Metla31 <Metlaiceblade@yandex.ru>
2022-03-24 21:34:35 +0300

    homework7/2

--- js/data.js
+++ js/data.js
@@ -83,3 +83,4 @@ for (let i = 0; i < AMTPOSTS; i++) {
 
 export {createPosts};
 export {posts};
+export {createComments};
--- js/postpreview.js
+++ js/postpreview.js
@@ -1,15 +1,42 @@
 const bigPost = (post) => {
   const b = document.querySelector('.big-picture');
   b.classList.remove('hidden');
+  document.body.classList.add('modal-open');
   const bigPicture = b.querySelector('.big-picture__img');
-  const bigPictureImg = bigPicture.querySelector('img')[0];
+  const bigPictureImg = bigPicture.querySelector('img');
   bigPictureImg.setAttribute('src', post.url);
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < post.comments.length && i < 10; i++) {
+    fragment.appendChild(buildComments(post.comments[i]));
+  };
+  b.querySelector('.likes-count').textContent = post.likes;
+  b.querySelector('.social__caption').textContent = post.description;
+  b.querySelector('.comments-count').textContent = post.comments.length;
+  b.querySelector('.social__comments').innerHTML = '';
+  b.querySelector('.social__comments').appendChild(fragment);
 };
 
+const PictureCloseButtonEsc = (evt) => {
+  if (evt.keyCode === 27) {
+    document.querySelector('.big-picture').classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }
+};
+
+const PictureCloseButton = document.querySelector('.big-picture__cancel');
+document.addEventListener('keydown', PictureCloseButtonEsc);
+PictureCloseButton.addEventListener('click', function () {
+  document.querySelector('.big-picture').classList.add('hidden');
+});
+
+const buildComments = (createComments) => {
+  const element = templateComment.cloneNode(true);
+  element.querySelector('.social__text').textContent = createComments.MESSAGES;
+  element.querySelector('.social__picture')
+    .setAttribute('src', (createComments.avatar));
+
+};
+
+export {PictureCloseButton};
 export {bigPost};
-
-
-//  b.querySelector('.likes-count').textContent = post.likes;
-//  b.querySelector('.social__caption').textContent = post.description;
-//  b.querySelector('.comments-count').textContent = post.comments.length;
-//  b.querySelector('.social__comments').innerHTML = '';
+console.log();
